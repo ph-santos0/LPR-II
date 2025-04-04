f0c9e2030410876cbd8e72b7607f19d79981020d
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-04-04 07:55:47 -0300

    Add files via upload

---
+++ Aula 1 - Introdução ao C#/Aula1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a primeira aula de C#");
+            Console.Write("\nDigite seu nome: ");
+            String nome = Console.ReadLine();
+            Console.Write("Digite sua idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nOlá {0}, sua idade é {1}", nome, idade);
+        }
+    }
+}
