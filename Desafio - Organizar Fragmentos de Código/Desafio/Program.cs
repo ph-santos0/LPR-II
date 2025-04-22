144d29efa47ff7380a40d136f09c16118a9cbe00
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-04-22 19:21:05 -0300

    Add files via upload

---
+++ Desafio - Organizar Fragmentos de Código/Desafio/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            string resultado = "";
+
+            while (x > 0)
+            {
+                if (x > 2)
+                {
+                    resultado = resultado + "a";
+                }
+
+                if (x == 2)
+                {
+                    resultado = resultado + "b c";
+                }
+
+                if (x == 1)
+                {
+                    resultado = resultado + "d";
+                    x = x - 1;
+                    continue;
+                }
+
+                resultado = resultado + "-";
+                x = x - 1;
+            }
+
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
