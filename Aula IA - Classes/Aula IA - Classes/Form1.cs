de9f2ee0ee369823ce3a447265f704a4be35821f
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-05-30 08:37:14 -0300

    Add files via upload

---
+++ Aula IA - Classes/Aula IA - Classes/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Aula_IA___Classes
+{
+    public partial class Form1 : Form
+    {
+        public Fusca fusca;
+        public Form1()
+        {
+            InitializeComponent();
+            fusca = new Fusca();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fusca fusca = new Fusca();
+            fusca.Aro = Convert.ToInt32(comboBox1.Text);
+            fusca.Placa = textBox1.Text;
+            fusca.CorLataria = textBox2.Text;
+            fusca.CorParachoque = textBox3.Text;
+            fusca.TipoVidro = comboBox2.Text;
+            MessageBox.Show("Dados cadastrados com sucesso!", "Cadastro");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(fusca.imprimir(), "Dados do fusca");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            fusca.Acelerar();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            fusca.Ligar();
+            label11.Text = "Ligado";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            fusca.Desigar();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            fusca.frear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fusca.TrocarMarcha();
+        }
+    }
+}
---
+++ Aula IA - Classes/Aula IA - Classes/Fusca.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Aula_IA___Classes
+{
+    public class Fusca
+    {
+        private string placa;
+        private string corLataria;
+        private string corParachoque;
+        private int aro;
+        private string tipoVidro;
+
+        public bool status = false;
+        public int velocidade = 0;
+        public int marcha = 0;
+
+        public Fusca()
+        {
+            placa = "";
+            corLataria = "";
+            corParachoque = "";
+            aro = 14;
+            tipoVidro = "comum";
+        }
+
+        public Fusca(string placa, string corLataria, string corParachoque, int aro, string tipoVidro)
+        {
+            this.placa = placa;
+            this.corLataria = corLataria;
+            this.corParachoque = corParachoque;
+            this.aro = aro;
+            this.tipoVidro = tipoVidro;
+        }
+
+        public string Placa { get => placa; set => placa = value; }
+        public string CorLataria { get => corLataria; set => corLataria = value; }
+        public string CorParachoque { get => corParachoque; set => corParachoque = value; }
+        public int Aro { get => aro; set => aro = value; }
+        public string TipoVidro { get => tipoVidro; set => tipoVidro = value; }
+
+        public void Ligar()
+        {
+            status = true;
+        }
+        public void Desigar()
+        {
+            status = false;
+        }
+        public void Acelerar()
+        {
+            velocidade += marcha * 10;
+        }
+        public void frear()
+        {
+            velocidade -= marcha * 10;
+        }
+        public void TrocarMarcha()
+        {
+            marcha = (marcha + 1) % 5;
+        }
+        public String imprimir()
+        {
+            String s = "";
+            s += placa + "\n" + corLataria + "\n" + corParachoque + "\n" + aro + "\n" + tipoVidro;
+            return s;
+        }
+    }
+}
