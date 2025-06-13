7bd5b14807d6f7b79d89848f8d01204724ad11c7
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-06-13 08:43:08 -0300

    Add files via upload

---
+++ Atividade - Compra Produtos/AulaPessoa/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AulaPessoa
+{
+    public class Produtos
+    {
+        private string[] p = {"Arroz","Feijão","Açucar","Leite","Sal","Macarrão"};
+        public string getProduto(int idProduto)
+        {
+            return p[idProduto];
+        }
+    }
+}
