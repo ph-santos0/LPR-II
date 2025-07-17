397cdf7470fdfae4b170aaad79a353a7484c1190
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-07-17 10:26:05 -0300

    Add files via upload

---
+++ Atividade 9 - Empregado/Empregado.cs
@@ -0,0 +1,30 @@
+﻿namespace OrientacaoObjetos
+{
+    public class Empregado
+    {
+        private string nomeEmp;
+        private int idadeEmp;
+
+        public Empregado()
+        {
+        }
+
+        public Empregado(string nomeEmp, int idadeEmp)
+        {
+            this.nomeEmp = nomeEmp;
+            this.idadeEmp = idadeEmp;
+        }
+
+        public int Idade
+        {
+            get { return idadeEmp; }
+            set { idadeEmp = value; }
+        }
+
+        public string Nome
+        {
+            get { return nomeEmp; }
+            set { nomeEmp = value; }
+        }
+    }
+}
