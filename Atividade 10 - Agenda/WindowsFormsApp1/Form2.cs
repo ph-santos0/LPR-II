9fdc88d63cf7433d71ecda5392a843d02e90767c
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-08-01 08:10:08 -0300

    Add files via upload

---
+++ Atividade 10 - Agenda/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.bDAgendaDataSet);
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'bDAgendaDataSet.Table'. Você pode movê-la ou removê-la conforme necessário.
+            this.tableTableAdapter.Fill(this.bDAgendaDataSet.Table);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.tableTableAdapter.FillByNome(this.bDAgendaDataSet.Table, textBox1.Text +"%");
+        }
+    }
+}
